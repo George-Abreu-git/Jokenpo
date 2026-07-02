cfd197c8e4c950d770e73790b26c90647d415823
George Abreu <georgeabreu68@outlook.com>
2026-07-02 00:17:17 -0300

    classe tourneament completada

--- Jokenpo/Models/Tourneament.cs
+++ Jokenpo/Models/Tourneament.cs
@@ -26,11 +26,54 @@ namespace Jokenpo.Models
             }
             
 
-            Console.WriteLine($"Campeão é {players}");
+            Console.WriteLine($"Campeão é {players[0]}");
             
         }
 
 
+        //Executa rodadas a partir partida
+        private static string[] JogarRodadas(string [][]pairs)
+        {
+          //atribui o tamanho de pairs em vencedores
+          string [] vencedores = new string[pairs.Length];
+
+          for (int i = 0; i < pairs.Length; i++)
+            {
+                vencedores[i] = Partida(pairs[i]);
+            }
+
+          return vencedores;  
+        }
+
+        //Executa apenas 1 partida entre 2 jogadores
+        private static string Partida(string [] pair)
+        {
+            //jogador1 e jogador 2 recebem 0 e 1 da lista de par
+            string jogador1 = pair[0];
+            string jogador2 = pair[1];
+
+            int jogada1 = 0;
+            int jogada2 = 0;
+            int resultado = 0;
+
+            //enquanto resultado for empate repete jogadas
+            while(resultado == 0)
+            {
+                jogada1 = Play.RandomPlay();
+                jogada2 = Play.RandomPlay();
+
+                resultado = Play.DetermineWinner(jogada1,jogada2);
+            }
+
+            if(resultado == jogada1)
+            {
+                return jogador1;   
+            }else
+            {
+                return jogador2;
+            }
+            
+        }
 
     }
 
